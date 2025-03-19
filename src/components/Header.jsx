7c972e750fdef3251f58d178dd3fee4813b6acfb
sandeepdot1 <sandeeppanigrahi3570@gmail.com>
2025-03-20 03:44:43 +0530

    updated header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,35 @@ const Header = ({ darkMode, setDarkMode }) => {
   return (
     <header className="header">
       <div className="container header-container">
-        <h1 style={{fontFamily: "sans-serif"}}>Sandeep Kumar Panigrahi</h1>
+        <h1 style={{ fontFamily: "sans-serif" }}>Sandeep Kumar Panigrahi</h1>
         <nav>
           <ul>
+            <li className="email-text">example@example.com</li>
             <li><a href="#about">About</a></li>
             <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
           </ul>
         </nav>
-        <div className="toggle-container">
-          <label className="switch">
-            <input type="checkbox" checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
-            <span className="slider round"></span>
-          </label>
+        <div className="header-right">
+          <div className="resume-container">
+            <a
+              href="https://drive.google.com/file/d/1R48BwG4prrY61fXbfgqP83CgRvBK9o9T/view?usp=sharing"  // Replace with your resume URL
+              target="_blank"
+              rel="noopener noreferrer"
+              className="resume-btn"
+            >
+              Resume
+            </a>
+          </div>
+          <div className="toggle-container">
+            <label className="switch">
+              <input
+                type="checkbox"
+                checked={darkMode}
+                onChange={() => setDarkMode(!darkMode)}
+              />
+              <span className="slider round"></span>
+            </label>
+          </div>
         </div>
       </div>
     </header>
