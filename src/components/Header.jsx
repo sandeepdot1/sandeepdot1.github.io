ea1644a04691cb98f9a16f6567bc64dd598f7afb
sandeepdot1 <sandeeppanigrahi3570@gmail.com>
2025-07-17 09:45:42 +0530

    Update resume link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = ({ darkMode, setDarkMode }) => {
         <div className="header-right">
           <div className="resume-container">
             <a
-              href="https://drive.google.com/file/d/1Yb6kYSkIJfLE3kc1L3e8FiQMoAqa4oFL/view?usp=sharing"  // Replace with your resume URL
+              href="https://drive.google.com/file/d/1G5tS3BlS9wONVWQyB9uLHcaVqHzqH56H/view?usp=sharing"  // Replace with your resume URL
               target="_blank"
               rel="noopener noreferrer"
               className="resume-btn"
